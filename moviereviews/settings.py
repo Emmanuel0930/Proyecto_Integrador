7b08576da45338b8dc624f4b509b98145d76469c
Emmanuel Alvarez Castrillon <ealvarezc1@eafit.edu.co>
2025-10-30 10:47:31 -0500

    actulizando

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-v(og^g)2lcb@-^e4lk+f1hf^gm(iy8vi0s0!)l@xjxh&^i=h4u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.160.242.231']
+ALLOWED_HOSTS = ['34.224.58.189']
 
 
 # Application definition
